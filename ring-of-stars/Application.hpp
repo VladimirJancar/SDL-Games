fe16789382f07a1360c50594a994f22944af8329
Vladimir Jancar <vlad.jancar@gmail.com>
2024-05-05 00:47:16 +0200

    added basic engine skeleton and image drawing

---
+++ ring-of-stars/Application.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <SDL.h>
+#include <iostream>
+
+class Application
+{
+public:
+    Application();
+    ~Application();
+
+    void loop();
+    void update(double delta_time);
+    void draw();
+
+private:
+    SDL_Surface* m_image;
+    SDL_Rect     m_image_position;
+
+    SDL_Window*  m_window;
+    SDL_Surface* m_window_surface;
+    SDL_Event    m_window_event;
+
+    double       m_image_x;
+    double       m_image_y;
+};
